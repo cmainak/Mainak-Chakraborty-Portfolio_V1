3ec25723eaa0050cbf2c4b7d23e511d194eed2c2
cmainak <113078695+cmainak@users.noreply.github.com>
2025-03-24 22:37:52 -0400

    commit

--- script.js
+++ script.js
@@ -51,6 +51,35 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
+    //Tool Scroll Animation
+
+
+        const toolsContent = document.querySelector('.tools-content');
+        const tools = Array.from(document.querySelectorAll('.tool-image'));
+    
+        // Clone the tool images to create a seamless loop
+        tools.forEach(tool => {
+            const clone = tool.cloneNode(true);
+            toolsContent.appendChild(clone);
+        });
+    
+        const toolWidth = tools[0].offsetWidth + 20; // Include margin
+        const totalWidth = toolWidth * tools.length;
+        let translateX = 0;
+        const slideSpeed = 2; // Increase the speed
+    
+        function slideTools() {
+            translateX -= slideSpeed;
+            if (translateX <= -totalWidth) {
+                translateX = 0;
+            }
+            toolsContent.style.transform = `translateX(${translateX}px)`;
+            requestAnimationFrame(slideTools);
+        }
+    
+        slideTools();
+
+
     
   
     // Scroll Animation Functionality
